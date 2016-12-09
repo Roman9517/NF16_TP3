8e679155be68d9b54f6b5cb44fff84c5a1671166
CapucinePr <capucine.prudhomme@gmail.com>
2016-12-09 17:15:33 +0100

    Create TP4.h

---
+++ TP4.h
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NMAX 20
+
+//declaration des structures
+
+typedef struct benevole
+{
+    char nom[NMAX];
+    char prenom[NMAX];
+    char sexe;
+    int CID;
+    int annee;
+    struct benevole *suivant;
+} Benevole;
+
+typedef struct ListeBenevole
+{
+    int nb;
+    Benevole *premier;
+} ListBenevoles;
+
+typedef struct Tranche
+{
+    ListBenevoles *liste;
+    int BorneSup;
+    struct Tranche *pere;
+    struct Tranche *flisG;
+    struct Tranche *filsD;
+} Tranche;
+
+//declaration structures rajoutées
+
+typedef struct ListeTranche
+{
+    Tranche *premier;
+    int nb;
+} ListeTranche;
+
+
+
+
+
+//prototypes
+
+Benevole *nouveauBen(char *nom, char *prenom, int CIN, char sexe, int annee);
+
+Tranche *nouvelleTranche(int borneSup);
+
+ListBenevoles *nouvelleListe();
+
+Tranche *ajoutTranche(Tranche *racine, int borneSup);
+
+Benevole *insererBen(Tranche *racine, Benevole *benevole);
+
+Benevole *chercherBen(Tranche *racine,int CIN, int *annee);
+
+int supprimerBen(Tranche *racine, int CIN, int annee);
+
+int supprimerTranche(Tranche *racine, int borneSup);
+
+ListBenevoles *BenDhonneur(Tranche *racine);
+
+int actualiser(Tranche *racine);
+
+int totalBenTranche(Tranche *racine, int borneSup);
+
+int totalBen(Tranche *racine);
+
+float pourcentageTranche(Tranche *racine, int borneSup);
+
+void afficherTranche(Tranche *racine, int borneSup);
+
+void afficherArbre(Tranche *racine);
+
+
+
+//prototypes fonctions ajoutées
+
+int attribuerBorne(int annee);
+
+ListBenevoles *insererlist(Tranche *tr, Benevole *bene);
+
+Tranche *chercherTranche(Tranche *racine, int Bornesup );
+
+int anneeActuelle();
+
+int bornemax(Tranche *racine);
+
+ListeTranche *creerListeTranche();
+
+void afficherBen(Benevole *ben);
